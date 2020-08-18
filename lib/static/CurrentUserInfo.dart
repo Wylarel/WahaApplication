3acc189dac31c3b80d60f1eb955f52b1a4123aa9
Wylarel <timjemine@gmail.com>
2020-08-19 00:00:41 +0200

    Add breaking stuff

---
+++ lib/static/CurrentUserInfo.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CurrentUserInfo {
+  static String uid;
+  static String fname;
+  static String surname;
+  static String displayName;
+
+  static void genFromUid(String _uid) async {
+    uid = _uid;
+    DocumentSnapshot documentSnapshot = await Firestore.instance.document('users/' + uid).get();
+    fname = documentSnapshot.data["fname"];
+    surname = documentSnapshot.data["surname"];
+    displayName = "$fname $surname";
+    print("Generating user info from uid | fname: $fname - surname: $surname - displayName: $displayName");
+  }
+}
